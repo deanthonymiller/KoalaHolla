0187f1555d4279829e2620aec7b75c3f7a5e6a55
Deanthony Miller <dmil1301@protonmail.com>
2018-07-06 11:19:54 -0500

    client push and get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,45 @@ const app = angular.module('KoalaApp', []);
 
 app.controller('KoalaController', ['$http', function($http){
 
+    let self = this;
+
+    self.petList = [];
+
+    //POST Koala
+    self.addKoala = function(koalaToAdd){
+        console.log(`adding Koala`, koalaToAdd);
+        
+        $http({
+            url:'/koala',
+            method:'POST',
+            data:koalaToAdd
+        }).then(function(res){
+            console.log(res);
+            self.getKoala()
+        }).catch(function(err){
+            console.log('ERROR!', err);
+        })
+    }
+
+    self.getKoala = function(){
+        $http({
+            url: '/koala',
+            method: 'GET'
+        }).then(function(res){
+            console.log(res)
+        }).catch(function(err){
+            console.log('Error in get', err)
+        })
+    }
+
+
+
+
+
+
+
+
+
+
 }]);
 
